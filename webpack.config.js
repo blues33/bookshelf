3870271fece9149be6661c428f414039f123ad87
Dmitriy <patrickjsmirnov@gmail.com>
2018-09-23 19:14:22 +0300

    webpack/babel

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const environment = process.env.NODE_ENV === 'production' ? 'production': 'development';
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+
+  mode: environment,
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              minimize: true,
+              modules: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
